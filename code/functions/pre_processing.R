19f1fba69a42c44701b7b37e974b32a315600e3b
Zihan (Lilac) Hong <103151283+LilacHo@users.noreply.github.com>
2026-04-17 14:57:40 -0700

    Add files via upload

---
+++ code/functions/pre_processing.R
@@ -0,0 +1,29 @@
+library(here)
+
+here::i_am("code/functions/pre_processing.R")
+
+input_file_path <- function(product, year, version){
+  
+  # Access the product raster
+  folder_name <- paste0("Annual_NLCD_", product, "_", year, "_CU_C1V", version)
+  file_name <- paste0("Annual_NLCD_", product,"_", year, "_CU_C1V",version,".tif")
+  full_file_name <- here::here("data", folder_name, file_name)
+  
+  return(full_file_name)
+  # Example: input_file_path(LndCov, 2023, 0)
+  # ->"data/Annual_NLCD_LndCov_2023_CU_C1V0/Annual_NLCD_LndCov_2023_CU_C1V0.tif"
+}
+
+
+input_file_path_server <- function(product, year, version){
+  
+  # Access the product raster
+  folder_name <- paste0("Annual_NLCD_", product, "_", year, "_CU_C1V", version)
+  file_name <- paste0("Annual_NLCD_", product,"_", year, "_CU_C1V",version,".tif")
+  full_file_name <- paste("D:/Shared/Scratch/Data/lilachong",product,folder_name,file_name, sep = "/")
+  
+  return(full_file_name)
+  # Example: input_file_path_server(LndCov, 2023, 0)
+  # ->"D:/Shared/Scratch/Data/lilachong/LndCov/Annual_NLCD_LndCov_2023_CU_C1V0/Annual_NLCD_LndCov_2023_CU_C1V0.tif"
+
+}
